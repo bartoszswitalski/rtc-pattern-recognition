f106b786d816b4336c3e580326417f6f84a0a0d7
Adrian Brodzik <adrianbrodzyk@gmail.com>
2020-11-15 17:13:48 +0100

    Crop camera feed to square and then scale down

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,6 +1,22 @@
+#include <algorithm>
 #include <opencv2/opencv.hpp>
 
 #include "pqueue.hpp"
+
+cv::Rect getCropROI(cv::VideoCapture *camera) {
+    cv::Mat img;
+    *camera >> img;
+
+    int w = img.size().width;
+    int h = img.size().height;
+
+    int size = std::min(w, h);
+
+    int dw = std::abs(w - size);
+    int dh = std::abs(h - size);
+
+    return cv::Rect(dw / 2, dh / 2, size, size);
+}
 
 int main() {
     cv::VideoCapture camera(-1);
@@ -10,43 +26,36 @@ int main() {
         return 1;
     }
 
-    cv::Mat img;
-
-    /*
-    while (cv::waitKey(10) != 27) {
-        camera >> img;
-        cv::resize(img, img, cv::Size(256, 256), 0, 0, CV_INTER_LINEAR);
-        uint8_t *pixels = (uint8_t *)img.data;
-        // cv::rotate(img, img, cv::ROTATE_90_CLOCKWISE);
-        // cv::rotate(img, img, cv::ROTATE_90_COUNTERCLOCKWISE);
-        // cv::rotate(img, img, cv::ROTATE_180);
-        cv::imshow("Generator", img);
-    };
-
-    // TODO: add the image to the queue
-    return 0;*/
+    cv::Rect roi = getCropROI(&camera);
 
     int shmidA = shmget(KEY_A, sizeof(PQueue<ImageRaw>), 0);
     PQueue<ImageRaw> *pqA = (PQueue<ImageRaw> *)shmat(shmidA, NULL, 0);
 
+    cv::Mat img;
     ImageRaw m;
 
     while (true) {
         camera >> img;
+        img = img(roi);
         cv::resize(img, img, cv::Size(256, 256), 0, 0, cv::INTER_LINEAR);
-        uint8_t *pixels = (uint8_t *)img.data;
 
-        memcpy(&m.data, pixels, sizeof(uint8_t) * IMG_SIZE);
+        memcpy(&m.data, img.data, sizeof(uint8_t) * IMG_SIZE);
+
+        // TODO:
+        //   should timestamp be set before mutex or inside mutex?
+        //   or multiple timestamp variables?
+        //   timestampInit, timestampPush, timestampPop
         m.timestamp = clock();
 
         down(pqA->getSemid(), EMPTY);
         down(pqA->getSemid(), BIN);
 
         pqA->push(&m);
-        std::cout << "[GENE] added value: " << (int)m.data[0] << std::endl;
 
         up(pqA->getSemid(), BIN);
         up(pqA->getSemid(), FULL);
+
+        std::cout << "[GENE] added value: " << (int)m.data[0] << std::endl;
     }
 
     return 0;

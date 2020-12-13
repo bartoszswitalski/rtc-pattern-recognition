348e2be20ff4e0bfd381fd6d1c0ffedc25a4fb86
Piotr Frątczak <piotrfratczak99@gmail.com>
2020-12-13 11:59:27 +0100

    Merge my local branch

--- include/semaphoreop.hpp
+++ include/semaphoreop.hpp
@@ -8,10 +8,4 @@
 
 void up(short unsigned int semid, short unsigned int semnum);
 
-<<<<<<< HEAD
-void up(int semid, int semnum);
-
-void down(int semid, int semnum);
-=======
 void down(short unsigned int semid, short unsigned int semnum);
->>>>>>> a35f22044aa4e9bac9e2913d09673b42bdf2d6bd
